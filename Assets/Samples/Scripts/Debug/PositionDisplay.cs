26f8d5af391165f72585b47e8e33d14aa7f9d98a
稲村 創 <so.inamura@gmail.com>
2017-06-18 01:54:53 +0900

    Add positional tracking camera scene

---
+++ Assets/Samples/Scripts/Debug/PositionDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionDisplay : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    void OnGUI()
+    {
+        if (target != null)
+        {
+            GUI.Label(new Rect(10, 10, 400, 30), "Camera position" + target.position);
+        }
+    }
+}
---
+++ Assets/Samples/Scripts/Random3DObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ランダムにプリミティブ型の3Dオブジェクトを作って配置する
+/// </summary>
+public class Random3DObject : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform centerPositionObject;
+
+    [SerializeField]
+    float maxLengthFromCenter;
+
+    [SerializeField]
+    float maxSize;
+
+    [SerializeField]
+    int numberOfObjects;
+
+    [SerializeField]
+    PrimitiveType[] primitiveTypes;
+
+    void Start()
+    {
+        var systemRandom = new System.Random();
+
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            var pos = Random.insideUnitSphere * maxLengthFromCenter;
+            var primitiveType = primitiveTypes[systemRandom.Next(primitiveTypes.Length)];
+
+            var go = GameObject.CreatePrimitive(primitiveType);
+			go.transform.parent = transform;
+            go.transform.position = centerPositionObject.transform.position + pos;
+            go.transform.localScale = Vector3.one * Random.Range(0.01f, maxSize);
+        }
+    }
+}
