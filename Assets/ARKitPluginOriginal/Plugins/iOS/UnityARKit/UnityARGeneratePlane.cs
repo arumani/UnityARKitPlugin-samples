8de693653a5d1ddf2e4dceafa82062cca0363677
稲村 創 <so.inamura@gmail.com>
2017-06-25 15:36:15 +0900

    Update GeneratePlane prefab to display debug data

--- Assets/ARKitPluginOriginal/Plugins/iOS/UnityARKit/UnityARGeneratePlane.cs
+++ Assets/ARKitPluginOriginal/Plugins/iOS/UnityARKit/UnityARGeneratePlane.cs
@@ -1,33 +1,44 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 
 namespace UnityEngine.XR.iOS
 {
-	public class UnityARGeneratePlane : MonoBehaviour
-	{
-		public GameObject planePrefab;
+    public class UnityARGeneratePlane : MonoBehaviour
+    {
+        [Tooltip("検出した平面情報から生成するPrefab。設定しない場合空のGameObjectのみ作られる")]
+        public GameObject planePrefab;
+
+        [Tooltip("検出した全ての平面情報をデバッグ表示するか")]
+        public bool displayPositions;
+
         private UnityARAnchorManager unityARAnchorManager;
 
-		// Use this for initialization
-		void Start () {
+        void Start()
+        {
             unityARAnchorManager = new UnityARAnchorManager();
-			UnityARUtility.InitializePlanePrefab (planePrefab);
-		}
+            UnityARUtility.InitializePlanePrefab(planePrefab);
+        }
 
         void OnDestroy()
         {
-            unityARAnchorManager.Destroy ();
+            unityARAnchorManager.Destroy();
         }
 
         void OnGUI()
         {
-            List<ARPlaneAnchorGameObject> arpags = unityARAnchorManager.GetCurrentPlaneAnchors ();
-            if (arpags.Count >= 1) {
-                //ARPlaneAnchor ap = arpags [0].planeAnchor;
-                //GUI.Box (new Rect (100, 100, 800, 60), string.Format ("Center: x:{0}, y:{1}, z:{2}", ap.center.x, ap.center.y, ap.center.z));
-                //GUI.Box(new Rect(100, 200, 800, 60), string.Format ("Extent: x:{0}, y:{1}, z:{2}", ap.extent.x, ap.extent.y, ap.extent.z));
+            if (displayPositions)
+            {
+                var sb = new StringBuilder();
+                foreach(var arpag in unityARAnchorManager.GetCurrentPlaneAnchors())
+                {
+                    var ap = arpag.planeAnchor;
+                    sb.AppendFormat("Center: x:{0}, y:{1}, z:{2} - ", ap.center.x, ap.center.y, ap.center.z);
+                    sb.AppendFormat("Extent: x:{0}, y:{1}, z:{2}\n", ap.extent.x, ap.extent.y, ap.extent.z);
+                }
+                GUI.Label(new Rect(10, 40, 800, 500), sb.ToString());
             }
         }
-	}
+    }
 }
 
