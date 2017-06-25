6af65cb2df151b6f4c4ee1a65b06a92b7f562c7e
稲村 創 <so.inamura@gmail.com>
2017-06-25 22:10:12 +0900

    Update debug menu & screen

--- Assets/Samples/Scripts/Debug/PositionDisplay.cs
+++ Assets/Samples/Scripts/Debug/PositionDisplay.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PositionDisplay : MonoBehaviour
 {
@@ -17,22 +18,43 @@ public class PositionDisplay : MonoBehaviour
 
     GUIStyle guiStyle;
 
-    void Start()
+    void OnEnable()
     {
         guiStyle = new GUIStyle();
         guiStyle.fontSize = 16;
     }
 
+    IEnumerator Start()
+    {
+        enabled = false;
+        // 誤タップ防止
+        yield return new WaitForSeconds(0.5f);
+        enabled = true;
+    }
+
     void OnGUI()
     {
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+        GUILayout.BeginHorizontal();
+
         if (targets != null)
         {
             var sb = new StringBuilder();
-            foreach(var t in targets)
+            foreach (var t in targets)
             {
                 sb.AppendFormat(t.label + " " + t.transform.position + "\n");
             }
             GUI.Label(new Rect(10, 10, 400, (targets.Length * 18 + 10)), sb.ToString(), guiStyle);
         }
+
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button("Return to Menu"))
+        {
+            SceneManager.LoadScene("00_Menu");
+        }
+
+        GUILayout.EndHorizontal();
+        GUILayout.EndArea();
     }
 }
---
+++ Assets/Samples/Scripts/Launcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Launcher : MonoBehaviour
+{
+    [SerializeField]
+    string[] sceneNames;
+
+    IEnumerator Start()
+    {
+        enabled = false;
+    	// 誤タップ防止
+        yield return new WaitForSeconds(0.5f);
+        enabled = true;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.BeginVertical();
+
+        foreach (var sceneName in sceneNames)
+        {
+            if (GUILayout.Button(sceneName))
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+
+        GUILayout.EndVertical();
+        GUILayout.EndHorizontal();
+        GUILayout.EndArea();
+    }
+}
