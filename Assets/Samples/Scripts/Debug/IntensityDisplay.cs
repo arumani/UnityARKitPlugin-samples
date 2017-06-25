912bcae5531a6017237157288309bb5a614c39ac
稲村 創 <so.inamura@gmail.com>
2017-06-25 21:18:48 +0900

    Add scenes(PointCloud / HitTest / Intensity)

---
+++ Assets/Samples/Scripts/Debug/IntensityDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class IntensityDisplay : MonoBehaviour
+{
+    GUIStyle guiStyle;
+    UnityARSessionNativeInterface m_Session;
+
+    void Start()
+    {
+        guiStyle = new GUIStyle();
+        guiStyle.fontSize = 20;
+#if !UNITY_EDITOR
+		m_Session = UnityARSessionNativeInterface.GetARSessionNativeInterface ();
+#endif
+    }
+
+
+    void OnGUI()
+    {
+#if !UNITY_EDITOR
+        float newai = m_Session.GetARAmbientIntensity();
+        GUI.Label(new Rect(10, 40, 400, 80), "Intensity " + (newai / 1000.0f), guiStyle);
+#endif
+    }
+}
