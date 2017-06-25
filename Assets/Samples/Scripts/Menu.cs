21828dcbb7224f2fcf156501d505a34adbc51d39
稲村 創 <so.inamura@gmail.com>
2017-06-25 15:37:09 +0900

    Add Menu scene

---
+++ Assets/Samples/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+	public Canvas menuButtons;
+	public Canvas returnButton;
+
+	string currentSceneName;
+
+    public void OpenScene(string sceneName)
+    {
+		menuButtons.gameObject.SetActive(false);
+		currentSceneName = sceneName;
+		SceneManager.LoadScene("Samples/Scenes/" + sceneName, LoadSceneMode.Additive);
+		StartCoroutine(WaitAndActive(returnButton.gameObject));
+    }
+
+	public void ReturnToMenu(){
+		returnButton.gameObject.SetActive(false);
+		SceneManager.UnloadSceneAsync(currentSceneName);
+		StartCoroutine(WaitAndActive(menuButtons.gameObject));
+	}
+
+	/// <summary>誤タップ防止で少し待つ</summary>
+	IEnumerator WaitAndActive(GameObject go){
+		yield return new WaitForSeconds(1);
+		go.SetActive(true);
+	}
+}
